665fb1d3f070cc1fecf079ef0b6f3e15b2e55dc0
uehara-n <na.ue0426@gmail.com>
2018-08-11 20:54:26 +0900

    googlmap

---
+++ pc/raiten.php
@@ -0,0 +1,35 @@
+<div id="raiten_yoyaku">
+<div class="img_box">
+  <img src="/wp-content/themes/reform/page_image/raiten/mainvisual.jpg" alt="WEBで来店予約">
+  <img src="/wp-content/themes/reform/page_image/raiten/tit_merit.png" alt="トマトハウスにご来店いただく3つのメリット">
+  <img src="/wp-content/themes/reform/page_image/raiten/pic_list.jpg" alt="写真リスト">
+</div>
+<div id="map" class="border_box flex_area">
+  <div class="map_inner">
+    <h3>トマトハウス本店</h3>
+       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3234.975660464209!2d139.39950971526284!3d35.82507288016103!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6018df292762aeef%3A0x5bccdd6d079d5b02!2z44CSMzU4LTAwMDIg5Z-8546J55yM5YWl6ZaT5biC5p2x55S677yX5LiB55uu77yS4oiS77yY!5e0!3m2!1sja!2sjp!4v1531378955619" width="320" height="280" frameborder="0" style="border:0" allowfullscreen></iframe>
+    <div class="text">
+      <p>〒358-0002<br />埼玉県入間市東町７丁目２－８</p>
+    </div>
+    <p class="btn"><a href="https://goo.gl/maps/dvjD2Z4wLjT2" target="_blank">Google Mapで確認する</a></p>
+  </div>
+  <div class="map_inner">
+    <h3>サイエンスホーム入間店</h3>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3233.8263171052363!2d139.3809987002785!3d35.85326213005709!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6018d88d0991ba6d%3A0xde0b382125e46bfd!2z44CSMzUwLTEzMjUg5Z-8546J55yM54ut5bGx5biC5qC55bK477yR5LiB55uu77yS77yY!5e0!3m2!1sja!2sjp!4v1533985808219" width="320" height="280" frameborder="0" style="border:0" allowfullscreen></iframe>
+    <div class="text">
+      <p>〒350-1325<br />埼玉県狭山市根岸１丁目２８</p>
+    </div>
+    <p class="btn"><a href="https://goo.gl/maps/UN5DMr59nfS2" target="_blank">Google Mapで確認する</a></p>
+  </div>
+</div>
+<div id="request_area">
+  <h3><img src="/wp-content/themes/reform/page_image/raiten/tit_raiten.png" alt="来店予約フォーム"></h3>
+    <p class="text">お問い合わせ内容を下記フォームに入力、送信してください。</p>
+<div class="border_box">
+<div id="toi_table_sp">
+  [contact-form-7 id="16132" title="来店予約"]
+</div>
+</div>
+</div>
+<!--request_area-->
+</div>
